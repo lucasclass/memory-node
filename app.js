9d7cbf1a59c6f4b2db5b48b31371a932cc093e5c
Lucas Class <lucasclass94@gmail.com>
2025-04-23 13:44:27 -0300

    feat: testando api

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use('/teste',(req, res, next) =>
+{
+  res.status(200).send({
+    mensagem: 'Ok, CORRETAMENTE'
+  })
+})
+
+module.exports = app;
