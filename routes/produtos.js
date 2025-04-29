5cdbf9d56c3672722e14aad27b852eafaa10e190
Lucas Class <lucasclass94@gmail.com>
2025-04-29 17:40:37 -0300

    test: rota para id especial

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// retorna todos os produtos
 router.get('/', (req, res, next) =>{
   res.status(200).send({
     mensagem: 'Usando o GET de PRODUTOS'
   })
 });
 
+// insere um produto
 router.post('/', (req, res, next) =>{
   res.status(201).send({
     mensagem: 'Usando POST de PRODUTOS'
@@ -14,7 +16,19 @@ router.post('/', (req, res, next) =>{
 });
 
 router.get('/:id_produto', (req, res, next) =>{
+  const id = req.params.id_produto
   
+  if (id === 'especial') {
+    res.status(200).send({
+      mensagem: 'Voce descobriu o ID especial',
+      id: id
+    });
+  } else {
+    res.status(200).send({
+      mensagem: 'Usando o GET de um produto exlusivo',
+      id: id
+    })
+  }
 })
 
 module.exports = router;
